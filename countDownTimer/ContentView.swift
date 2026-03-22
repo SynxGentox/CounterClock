e1b221b3ad3cbc7d6c8a139c479f3dc7eaed6345
SynxGentox <varyan069@gmail.com>
2026-03-23 01:37:37 +0530

    alpha v0.001.
    Ui logic needs to be re written

--- countDownTimer/ContentView.swift
+++ countDownTimer/ContentView.swift
@@ -10,8 +10,8 @@ import Observation
 
 @Observable
 class CountDownLogic {
-    var sec: Int = 59
-    var min: Int = 1
+    var sec: Int = 0
+    var min: Int = 0
     var isRunning: Bool = false
     var timer: Timer?
     
@@ -22,17 +22,18 @@ class CountDownLogic {
     
     func startTimer() {
         if isRunning {
-           timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-               if self.sec > 0 {
-                   self.sec -= 1
-               } else if self.min > 0 {
-                    self.sec = 59
-                   self.min -= 1
-                } else {
-                    self.pauseTimer()
-                    self.isRunning.toggle()
+            timer = Timer
+                .scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                    if self.sec > 0 {
+                        self.sec -= 1
+                    } else if self.min > 0 {
+                        self.sec = 59
+                        self.min -= 1
+                    } else {
+                        self.pauseTimer()
+                        self.isRunning.toggle()
+                    }
                 }
-            }
         }
     }
     func pauseTimer() {
@@ -50,8 +51,40 @@ class CountDownLogic {
 struct ContentView: View {
     @Environment(CountDownLogic.self) private var timer
     var body: some View {
+        TabView {
+            Tab.init("Clock", systemImage: "clock.fill") {
+                
+            }
+            
+            Tab.init("Alarm", systemImage: "alarm.waves.left.and.right.fill") {
+                
+            }
+            
+            Tab.init("StopWatch", systemImage: "stopwatch.fill") {
+                CountDownView(selection: timer.sec) {
+                    ForEach (0...59,id: \.self) { value in
+                        
+                        Text(timer.displayTime)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CountDownView<Content: View, Selection: Hashable>: View {
+    @Environment(CountDownLogic.self) private var timer
+    @State var selection: Selection
+    @ViewBuilder var content: Content
+    
+    var body: some View {
         VStack {
-            Text(timer.displayTime)
+            Picker("", selection: $selection) {
+                content
+               // timer.sec = Int(selection)
+                
+            }
+            .pickerStyle(.wheel)
             
             Button("Start") {
                 if !timer.isRunning {
@@ -73,10 +106,13 @@ struct ContentView: View {
             
         }
         .padding()
+        .navigationTitle("StopWatch")
     }
 }
 
 #Preview {
-    ContentView()
-        .environment(CountDownLogic())
+    NavigationStack {
+        ContentView()
+    }
+    .environment(CountDownLogic())
 }
